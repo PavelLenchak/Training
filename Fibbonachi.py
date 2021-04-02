f8c4ee0ab91050f5ba8c32d5717c534266767fe5
PavelLenchak <80241166+PavelLenchak@users.noreply.github.com>
2021-04-02 18:41:46 +0600

    Function factorial was added

--- Fibbonachi.py
+++ Fibbonachi.py
@@ -9,5 +9,14 @@ def fido(num):
     else:
         return fido(num-1) + fido(num-2)
 
+def factorial(num):
+    if num == 0:
+        return 1
+    elif num == 1:
+        return 1
+    else:
+        return num * factorial(num-1)
+
 
 print(fido(10))
+print(factorial(6))
