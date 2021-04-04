99f62a7730132d53523c8d5820fdc4ef7e1778ee
PavelLenchak <80241166+PavelLenchak@users.noreply.github.com>
2021-04-04 13:09:06 +0600

    Update create game board.py

--- Pygame trainnig/create game board.py
+++ Pygame trainnig/create game board.py
@@ -3,23 +3,38 @@ import sys
 
 pygame.init()
 
-SCREEN = (510, 510)
+SIZE = (510, 510)
+SCREEN = pygame.display.set_mode(SIZE)
 IMG = pygame.image.load('Pygame trainnig\index.png')
-RED = (255, 0, 0)
-WHITE = (255, 255, 255)
-WIDTH = HIGHT = 40
-
-pygame.display.set_mode(SCREEN)
 pygame.display.set_icon(IMG)
 pygame.display.set_caption('Cross zero')
 
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+MARGIN = 10
+WIDTH = HIGHT = 40
+
+mas = [[0]*10 for i in range(10)]
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             quit()
-            sys.exit()
+            sys.exit(0)
         elif event.type == pygame.MOUSEBUTTONDOWN:
             x_mouse, y_mouse = pygame.mouse.get_pos()
             print('X pos = {}, Y pos = {}'.format(x_mouse,y_mouse))
+            column = x_mouse // (MARGIN + WIDTH)
+            row = y_mouse // (MARGIN + HIGHT)
+            mas[row][column] ^= 1
 
+    for row in range(10):
+        for col in range(10):
+            if mas[row][col] == 1:
+                color = RED
+            else:
+                color = WHITE
+            x = col * WIDTH + (col+1) * MARGIN
+            y = row * HIGHT + (row+1) * MARGIN
+            pygame.draw.rect(SCREEN, color, (x, y, WIDTH, HIGHT))
+    pygame.display.update()
