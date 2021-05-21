c02549deb9149092a468ea7c38e9f02e32253b81
PavelLenchak <80241166+PavelLenchak@users.noreply.github.com>
2021-05-21 18:56:54 +0600

    Parsing is done

--- Parsing/chandeliers/parsing chan.py
+++ Parsing/chandeliers/parsing chan.py
@@ -1,4 +1,4 @@
-import sys
+import sys, os
 import re
 import logging
 from datetime import datetime
@@ -17,6 +17,9 @@ HOST = 'http://www.thornlighting.ru'
 URL = 'http://www.thornlighting.ru/ru-ru/produkty/vnutriennieie-osvieshchieniie'
 CSV_FILE = 'Parsing\\chandeliers\\products.csv'
 
+DOCS_PATH = 'Parsing\\chandeliers\\docs'
+DOCS_FILE = 'Parsing\\chandeliers\\docs.csv'
+
 logging.basicConfig(filename='Parsing\\chandeliers\\logs.csv', level=logging.INFO)
 
 HEADERS = {
@@ -25,12 +28,29 @@ HEADERS = {
 
 def save_csv(items, path):
     titels = list(items[0].keys())
-    with codecs.open(path, 'a', encoding='utf-32') as csv_file:
+    with open(path, 'a', newline='') as csv_file:
         writer = csv.writer(csv_file, delimiter=';')
         for item in items:
             task = [str(item[titels[i]]).replace('.', ',') for i in range(len(titels))]
             writer.writerow(task)
     logging.info('Парсим {} {} в категориях {} {}'.format(items[0]['Модификация'], items[0]['Серия'], items[0]['Раздел 2'], items[0]['Раздел 1']))
+
+def save_docs(url, file_name, file_type, sap_code):
+    path = f'{DOCS_PATH}\{sap_code}'
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+    #print(path)
+    ufr = requests.get(url)
+    with open(f'{path}\{file_name}{file_type}',"wb") as file: #открываем файл для записи, в режиме wb
+        file.write(ufr.content) #записываем содержимое в файл; как видите - content запроса
+    
+    with open(DOCS_FILE, 'a', newline='') as doc_file:
+        writer = csv.writer(doc_file, delimiter=';')
+        items = [sap_code, f'{file_name}{file_type}']
+        writer.writerow(items)
+    
+    logging.info('Записан файл {} {}'.format(sap_code, file_name))
 
 
 def clean_html(raw_html):
@@ -66,11 +86,27 @@ def get_modes(url, titels):
     serial_name = titels[2]
     serial_description = titels[3]
 
-    try:
-        img_url = soup.find('div', class_='portletImages').find('img').get('src')
-    except:
-        print('IMG ERROR')
-        img_url = 'IMG ERROR'
+    # Ссылки на все документы страницы
+    docs_url = []
+    options = soup.find_all('div', class_='option')
+    for option in options:
+        docs_url.append({
+            'url': option.find('a').get('href'),
+            'type': option.find('a', {'target': '_blank'}).get_text()
+            })
+    
+    # Сохрагяем файлы по ссылкам
+    for dicti in docs_url:
+        url = dicti['url']
+        file_name = dicti['type'][:dicti['type'].find('(')-1]
+        type = dicti['type'][dicti['type'].find('.'):dicti['type'].find(')')]
+        save_docs(url, file_name=file_name, file_type=type, sap_code=sap_code)
+
+    # try:
+    #     img_url = soup.find('div', class_='portletImages').find('img').get('src')
+    # except:
+    #     print('IMG ERROR')
+    #     img_url = 'IMG ERROR'
 
     try:
         p = list(soup.find('div', class_='wrapper').find('p'))
@@ -121,9 +157,10 @@ def get_modes(url, titels):
         'Световой поток светильника': light,#p[7][p[7].find(':')+2:],
         'КПД светильника': kpd,#p[9][p[9].find(':')+2:],
         'Вес': weight,#p[11][p[11].find(':')+2:],
-        'IMG': img_url,
-        'Datas': span_right
     })
+
+    for index, span in enumerate(span_left):
+        datas[0][span] = span_right[index]
     save_csv(datas, CSV_FILE)
     datas=[]
 
@@ -181,6 +218,9 @@ def get_content(html):
                 ]
 
             print(f'ССЫЛОК НА МОДИФИКАЦИИ {len(all_mod_urls)}')
+            # for url in all_mod_urls:
+            #     get_modes(url, datas_to_save)
+            
             with Pool(10) as p:
                 p.map(functools.partial(get_modes, titels=datas_to_save), all_mod_urls)
 
