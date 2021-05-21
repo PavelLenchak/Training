3a8cc426b9cdc4f9278f30a03111fa7f3677a0bd
PavelLenchak <80241166+PavelLenchak@users.noreply.github.com>
2021-05-21 07:18:14 +0600

    Milti parsing

--- Parsing/chandeliers/parsing chan.py
+++ Parsing/chandeliers/parsing chan.py
@@ -8,6 +8,7 @@ from bs4 import BeautifulSoup
 import csv
 from multiprocessing import Pool
 import codecs
+import functools
 
 # Фото Свойтво товаров Документы
 # <img src="http://www.thornlighting.ru/img/31/tlg_thumb_downlights/@@images/c47c664c-1c6f-4b2e-9622-51c23accdef6.jpeg" alt="Светильники типа downlight" title="" height="176" width="176">
@@ -37,9 +38,87 @@ def clean_html(raw_html):
     cleantext = re.sub(cleanr, '', raw_html)
     return cleantext
 
-def get_html(url, params=''):
-    req = requests.get(url, params=params)
+def get_html(url):
+    req = requests.get(url)
     return req
+
+def test(url, titels):
+    print(url)
+    print(titels)
+    sys.exit()
+
+def get_modes(url, titels):
+    datas = []
+    html = get_html(url).text
+    soup = BeautifulSoup(html, 'html.parser')
+
+    sap_code = soup.find('div', id='article-detail').find('span').get_text()
+    modification = soup.find('span', id='breadcrumbs-current').get_text()
+    version = soup.find('span', id='breadcrumbs-5').find('a').get_text()
+    main_chapter = titels[0]
+    sub_chapter = titels[1]
+    serial_name = titels[2]
+    serial_description = titels[3]
+
+    try:
+        img_url = soup.find('div', class_='portletImages').find('img').get('src')
+    except:
+        print('IMG ERROR')
+        img_url = 'IMG ERROR'
+
+    try:
+        p = list(soup.find('div', class_='wrapper').find('p'))
+    except:
+        try:
+            p = list(soup.find('div', class_='row Spacer').find('p'))
+        except:
+            logging.info(f'P find ERROR {main_chapter} {sub_chapter} {serial_name} {url}')
+            print(f'P find ERROR {main_chapter} {sub_chapter} {serial_name}')
+            
+    try:
+        size = p[3][p[3].find(':')+2:]
+    except:
+        size = ''
+    try:
+        power = p[5][p[5].find(':')+2:]
+    except:
+        power = ''
+    try:
+        light = p[7][p[7].find(':')+2:]
+    except:
+        light = ''
+    try:
+        kpd = p[9][p[9].find(':')+2:]
+    except:
+        kpd = ''
+    try:
+        weight = p[11][p[11].find(':')+2:]
+    except:
+        weight = ''
+    try:
+        common_datas = p[0]
+    except:
+        common_datas = ''
+
+    print(f'Парсим {modification} | {serial_name} | {main_chapter}')
+    datas.append({
+        'Раздел 1': main_chapter,
+        'Раздел 2': sub_chapter,
+        'Серия': serial_name,
+        'Описание серии': serial_description,
+        'Версия': version,
+        'Модификация': modification,
+        'SAP CODE': sap_code,
+        'Общие данные': common_datas,#p[0],
+        'Размеры': size,#p[3][p[3].find(':')+2:],
+        'Номинальная мощность': power,#p[5][p[5].find(':')+2:],
+        'Световой поток светильника': light,#p[7][p[7].find(':')+2:],
+        'КПД светильника': kpd,#p[9][p[9].find(':')+2:],
+        'Вес': weight,#p[11][p[11].find(':')+2:],
+        'IMG': img_url,
+    })
+    save_csv(datas, CSV_FILE)
+    datas=[]
 
 
 def get_content(html):
@@ -70,7 +149,6 @@ def get_content(html):
                 'url': item.get('href'),
                 })
         
-        datas = []
         # Кликаем на каждый товар
         for pr in products:
             serial_name = pr['name']
@@ -78,6 +156,7 @@ def get_content(html):
             soup = BeautifulSoup(html, 'html.parser')
             all_tr_even = soup.find_all('tr', class_='even')
             all_tr_odds = soup.find_all('tr', class_='odd')
+            #print(f'Take {serial_name}')
 
             serial_description = soup.find('div', class_='row rowSpacer').get_text()
             
@@ -87,73 +166,84 @@ def get_content(html):
             for tr_odd in all_tr_odds:
                 all_mod_urls.append(tr_odd.find('a', class_='article_link').get('href'))
 
+            datas_to_save = [
+                main_chapter,
+                sub_chapter,
+                serial_name,
+                serial_description,
+                ]
+
+            print(f'ССЫЛОК НА МОДИФИКАЦИИ {len(all_mod_urls)}')
+            with Pool(10) as p:
+                p.map(functools.partial(get_modes, titels=datas_to_save), all_mod_urls)
+
             #<div class="wrapper" style="display: block;"><p>Встраиваемый светодиодный светильник.
-            for url in all_mod_urls:
-                html = get_html(url).text
-                soup = BeautifulSoup(html, 'html.parser')
+            # for url in all_mod_urls:
+            #     html = get_html(url).text
+            #     soup = BeautifulSoup(html, 'html.parser')
 
-                sap_code = soup.find('div', id='article-detail').find('span').get_text()
-                modification = soup.find('span', id='breadcrumbs-current').get_text()
-                version = soup.find('span', id='breadcrumbs-5').find('a').get_text()
+            #     sap_code = soup.find('div', id='article-detail').find('span').get_text()
+            #     modification = soup.find('span', id='breadcrumbs-current').get_text()
+            #     version = soup.find('span', id='breadcrumbs-5').find('a').get_text()
 
-                try:
-                    img_url = soup.find('div', class_='portletImages').find('img').get('src')
-                except:
-                    print('IMG ERROR')
-                    img_url = 'IMG ERROR'
+            #     try:
+            #         img_url = soup.find('div', class_='portletImages').find('img').get('src')
+            #     except:
+            #         print('IMG ERROR')
+            #         img_url = 'IMG ERROR'
 
-                try:
-                    p = list(soup.find('div', class_='wrapper').find('p'))
-                except:
-                    try:
-                        p = list(soup.find('div', class_='row Spacer').find('p'))
-                    except:
-                        pass
-                try:
-                    size = p[3][p[3].find(':')+2:]
-                except:
-                    size = ''
-                try:
-                    power = p[5][p[5].find(':')+2:]
-                except:
-                    power = ''
-                try:
-                    light = p[7][p[7].find(':')+2:]
-                except:
-                    light = ''
-                try:
-                    kpd = p[9][p[9].find(':')+2:]
-                except:
-                    kpd = ''
-                try:
-                    weight = p[11][p[11].find(':')+2:]
-                except:
-                    weight = ''
-                try:
-                    common_datas = p[0]
-                except:
-                    common_datas = ''
+            #     try:
+            #         p = list(soup.find('div', class_='wrapper').find('p'))
+            #     except:
+            #         try:
+            #             p = list(soup.find('div', class_='row Spacer').find('p'))
+            #         except:
+            #             pass
+            #     try:
+            #         size = p[3][p[3].find(':')+2:]
+            #     except:
+            #         size = ''
+            #     try:
+            #         power = p[5][p[5].find(':')+2:]
+            #     except:
+            #         power = ''
+            #     try:
+            #         light = p[7][p[7].find(':')+2:]
+            #     except:
+            #         light = ''
+            #     try:
+            #         kpd = p[9][p[9].find(':')+2:]
+            #     except:
+            #         kpd = ''
+            #     try:
+            #         weight = p[11][p[11].find(':')+2:]
+            #     except:
+            #         weight = ''
+            #     try:
+            #         common_datas = p[0]
+            #     except:
+            #         common_datas = ''
 
             
-                print(f'Парсим {modification} | {serial_name} | {main_chapter}')
-                datas.append({
-                    'Раздел 1': main_chapter,
-                    'Раздел 2': sub_chapter,
-                    'Серия': serial_name,
-                    'Описание серии': serial_description,
-                    'Версия': version,
-                    'Модификация': modification,
-                    'SAP CODE': sap_code,
-                    'Общие данные': common_datas,#p[0],
-                    'Размеры': size,#p[3][p[3].find(':')+2:],
-                    'Номинальная мощность': power,#p[5][p[5].find(':')+2:],
-                    'Световой поток светильника': light,#p[7][p[7].find(':')+2:],
-                    'КПД светильника': kpd,#p[9][p[9].find(':')+2:],
-                    'Вес': weight,#p[11][p[11].find(':')+2:],
-                    'IMG': img_url,
-                })
-                save_csv(datas, CSV_FILE)
-                datas=[]
+            #     print(f'Парсим {modification} | {serial_name} | {main_chapter}')
+            #     datas.append({
+            #         'Раздел 1': main_chapter,
+            #         'Раздел 2': sub_chapter,
+            #         'Серия': serial_name,
+            #         'Описание серии': serial_description,
+            #         'Версия': version,
+            #         'Модификация': modification,
+            #         'SAP CODE': sap_code,
+            #         'Общие данные': common_datas,#p[0],
+            #         'Размеры': size,#p[3][p[3].find(':')+2:],
+            #         'Номинальная мощность': power,#p[5][p[5].find(':')+2:],
+            #         'Световой поток светильника': light,#p[7][p[7].find(':')+2:],
+            #         'КПД светильника': kpd,#p[9][p[9].find(':')+2:],
+            #         'Вес': weight,#p[11][p[11].find(':')+2:],
+            #         'IMG': img_url,
+            #     })
+            #     save_csv(datas, CSV_FILE)
+            #     datas=[]
 
 def do_all(each_url):
     html = get_html(each_url)
@@ -169,8 +259,10 @@ def main():
         'http://www.thornlighting.ru/ru-ru/produkty/sistiemy-upravlieniia-osvieshchieniiem-i-avariinoie-osvieshchieniie'
     ]
     
-    with Pool(3) as p:
-        p.map(do_all, main_url)
+    for url in main_url:
+        do_all(url)
+    # with Pool(3) as p:
+    #     p.map(do_all, main_url)
 
     end = datetime.now()
     logging.info('Закончили. Время выполнения - {}'.format(end - start))
