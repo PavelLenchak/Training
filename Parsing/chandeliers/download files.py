1667df10e98eddb60e592bea6845a944fe51e71f
PavelLenchak <80241166+PavelLenchak@users.noreply.github.com>
2021-05-23 15:29:47 +0600

    Can parsing main and sub information

--- Parsing/chandeliers/download files.py
+++ Parsing/chandeliers/download files.py
@@ -4,18 +4,19 @@ from typing import Counter
 from bs4 import BeautifulSoup
 import requests
 import csv
-import os, sys
+import os, sys, time
 from datetime import datetime
 import logging
 import re
 from fake_useragent import UserAgent
 import codecs
 import string, random
-from multiprocessing import Pool
+from multiprocessing import Pool, cpu_count
 from progress.bar import IncrementalBar
 import io, logging
 
-logging.basicConfig(filename='Parsing\\chandeliers\\saving_process_logs.csv', level=logging.INFO)
+# logging.basicConfig(filename='Parsing\\chandeliers\\saving_process_logs.csv', level=logging.INFO)
+logging.basicConfig(filename='Parsing\\chandeliers\\docs_process.csv', level=logging.INFO)
 
 HEADERS = {
     'user-agent': UserAgent().chrome}
@@ -23,9 +24,11 @@ HEADERS = {
 DOCS_FILE = 'Parsing\\chandeliers\\docs.csv'
 
 URLS = 'Parsing\\chandeliers\\files\\urls.csv'
-TEST_URL_TO_SAVE = 'D:\\Python\\docs'
-MAIN_FILE = 'Parsing\\chandeliers\\files\MAIN FILE.csv'
+TEST_DIR_TO_SAVE = 'D:\\Python\\docs'
+MAIN_FILE = 'Parsing\\chandeliers\\to post\\MAIN FILE.csv'
 SUB_FILE = 'Parsing\\chandeliers\\SUB FILE.csv'
+
+TO_PARSING = 'Parsing\\chandeliers\\files\\to_parsing.csv'
 
 def clean_html(raw_html):
     cleanr = re.compile('<.*?>')
@@ -33,36 +36,33 @@ def clean_html(raw_html):
     return cleantext
 
 def get_html(url):
-    req = requests.get(url, headers=HEADERS)
-    req.encoding = 'utf8'
+    try:
+        time.sleep(2)
+        req = requests.get(url, headers=HEADERS)
+        req.encoding = 'utf8'
+    except requests.exceptions.ConnectionError:
+        print('EXEPTION ERROR')
+        time.sleep(20)
+        get_html(url)
+    
     return req
 
 #[{ }]
 def save_to_csv(items, path):
-    titels = list(items[0].keys())
-    # try:
+    try:
+        titels = list(items[0].keys())
+    except IndexError as ie:
+        titels = ['None']
+        logging.info('INDEX ERROR', ie)
+        print('INDEX ERROR', ie)
     with open(path, 'a', newline='') as csv_file:
         writer = csv.writer(csv_file, delimiter=';')
         for item in items:
-            task = [str(item[titels[i]]) for i in range(len(titels))]
+            task = [re.sub('[^A-Z a-z А-Я а-я 0-9 .\/;:-]', '', str(item[titels[i]])) for i in range(len(titels))]
             row_to_save = [re.sub('[^A-Z a-z А-Я а-я 0-9 .\/;:-]', '', str(task))]
-            print(row_to_save)
-            writer.writerow(row_to_save)
-        print(f'Saving {task[-2]}')
-    # except UnicodeEncodeError as ue:
-    #     with io.open(path, 'a', newline='',encoding='utf-8') as csv_file:
-    #         writer = csv.writer(csv_file, delimiter=';')
-    #         for item in items:
-    #             task = [str(item[titels[i]]).replace('.', ',') for i in range(len(titels))]
-    #             if type(task) != list or 'html' not in task:
-    #                 writer.writerow([re.sub(r'[^A-zА-я0-9]', '', str(task))])
-    #             else:
-    #                 writer.writerow(task)
-
-                # print(f'Saving {task[-2]}')
-        # logging.info('SAVING ERROR: ', ue)
-        # print('SAVING ERROR: ', ue)
-    print(f'Saving process have done')
+            writer.writerow(task)
+        print(f'Saving {task[-1]}')
+    #print(f'Saving process have done')
 
 def read_csv(path):
     urls = []
@@ -81,12 +81,12 @@ def save_docs(url, file_name, file_type, sap_code):
 
     #print(path)
     ufr = requests.get(url)
-    with open(f'{TEST_URL_TO_SAVE}\{sap_code}_{file_name}{file_type}',"wb") as file: #открываем файл для записи, в режиме wb
+    with open(f'{TEST_DIR_TO_SAVE}\{sap_code}_{file_name}{file_type}',"wb") as file: #открываем файл для записи, в режиме wb
         file.write(ufr.content) #записываем содержимое в файл; как видите - content запроса
     
     with open(DOCS_FILE, 'a', newline='') as doc_file:
         writer = csv.writer(doc_file, delimiter=';')
-        items = [sap_code, f'{sap_code}_{file_name}{file_type}']
+        items = [sap_code, f'{sap_code}_{file_name}{file_type}', url]
         writer.writerow(items)
     
     print('Записан файл {}_{}'.format(sap_code, file_name))
@@ -96,8 +96,12 @@ def get_files(url):
     html = get_html(url)
     soup = BeautifulSoup(html.text, 'html.parser')
 
-    sap_code = soup.find('div', id='article-detail').find('span', {'title': 'SAP Code'}).get_text()
-    #print(f'Парсим {sap_code}')
+    detail = soup.find('div', id='article-detail')
+    sap_code = clean_html(str(detail.find('p').find('span', {'title': 'SAP Code'}).get_text()))
+
+    #sap_code = soup.find('div', id='article-detail').find('span', {'title': 'SAP Code'}).get_text()
+    print(f'Парсим {sap_code}')
+    logging.info(f'Парсим {sap_code}')
 # Ссылки на все документы страницы
     docs_url = []
     options = soup.find_all('div', class_='option')
@@ -107,18 +111,15 @@ def get_files(url):
             'type': option.find('a', {'target': '_blank'}).get_text()
             })
     
-    bar = IncrementalBar('Progress', max = len(docs_url))
     # # Сохрагяем файлы по ссылкам
     for dicti in docs_url:
-        bar.next()
         url = dicti['url']
         #a = requests.get(url)
         #file_name = dicti['type'][:dicti['type'].find('(')-1]
 
-        file_name = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(10))
+        file_name = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(5))
         type = dicti['type'][dicti['type'].find('.'):dicti['type'].find(')')]
         save_docs(url, file_name=file_name, file_type=type, sap_code=sap_code)
-    bar.finish()
 
 
 def get_mode(url):
@@ -165,28 +166,30 @@ def get_mode(url):
             'Version': bc.find('span', id='breadcrumbs-5').find('a').get_text(),
             'Description extra': span,
             'SAP Code': clean_html(str(detail.find('p').find('span', {'title': 'SAP Code'}).get_text())),
-            'URL': clean_html(url.replace(',', '.')),
+            'URL': clean_html(url),
         })
         
     # print(bread_crumbs)
     # sys.exit()
-
+    logging.info(f'PARSING {url}')
     save_to_csv(bread_crumbs, SUB_FILE)
 
 
 def main():
     start = datetime.now()
     urls = read_csv(MAIN_FILE)
-    # counter = 0 
-    # for url in urls:
-    #     if counter < 2:
-    #         get_files(url)
-    #     counter += 1
-
+    counter = 0 
     for url in urls:
-        get_mode(url)
+        if counter < 2:
+            get_files(url)
+        counter += 1
 
-    # with Pool(40) as p:
+    # bar = IncrementalBar('Progress', max = len(urls))
+    # for url in urls:
+    #     bar.next()
+    #     get_mode(url)
+
+    # with Pool(cpu_count()) as p:
     #     p.map(get_mode, urls)
 
     # with Pool(40) as p:
