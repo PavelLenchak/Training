9e87dc0b6b97d13d6c65f60357a194c89de55a16
PavelLenchak <80241166+PavelLenchak@users.noreply.github.com>
2021-05-25 21:27:59 +0600

    Update get_sub_info.py

--- Parsing/lightstar/get_sub_info.py
+++ Parsing/lightstar/get_sub_info.py
@@ -4,10 +4,12 @@ import requests
 from bs4 import BeautifulSoup
 import fake_useragent
 import random, string
+from multiprocessing import Pool, cpu_count
+
 
 PATH = 'Parsing\\lightstar\\to post\\lightstar.csv'
 CSV_FILE = 'Parsing\\lightstar\\sub_info.csv'
-TO_SACE_DOCS = 'D:\\Python\\lightstart docs'
+TO_SAVE_DOCS = 'D:\\Python\\lightstart docs'
 DOCS_FILE = 'Parsing\\lightstar\\docs.csv'
 
 HEADERS = {
@@ -15,18 +17,28 @@ HEADERS = {
 }
 
 def save_url(url, file_name):
+    print(f'Saving {file_name}')
     ufr = requests.get(url)
     type = '.' + url.split('.')[-1]
+    #print(type)
     # print(type)
     # print(file_name)
     # sys.exit()
-    with open(f'{TO_SACE_DOCS}\{file_name}{type}',"wb") as file: #открываем файл для записи, в режиме wb
-        file.write(ufr.content) #записываем содержимое в файл; как видите - content запроса
-    
-    with open(DOCS_FILE, 'a', newline='') as doc_file:
-        writer = csv.writer(doc_file, delimiter=';')
-        items = [file_name.split('_')[0], f'{file_name}{type}', url]
-        writer.writerow(items)
+    try:
+        with open(f'{TO_SAVE_DOCS}\{file_name}{type}',"wb") as file: #открываем файл для записи, в режиме wb
+            file.write(ufr.content) #записываем содержимое в файл; как видите - content запроса
+    except FileNotFoundError:
+        print(f'ERROR SAVING')
+        with open(DOCS_FILE, 'a', newline='') as doc_file:
+            writer = csv.writer(doc_file, delimiter=';')
+            items = [file_name.split('_')[0], f'{file_name}{type}', url, "3D Model"]
+            writer.writerow(items)
+        pass
+    else:
+        with open(DOCS_FILE, 'a', newline='') as doc_file:
+            writer = csv.writer(doc_file, delimiter=';')
+            items = [file_name.split('_')[0], f'{file_name}{type}', url]
+            writer.writerow(items)
     
     print('Записан файл {}'.format(file_name))
     #logging.info('Записан файл {}_{} {}'.format(sap_code, file_name, url))
@@ -72,7 +84,7 @@ def get_content(url):
     soup = BeautifulSoup(html.text, 'html.parser')
     li = soup.find_all('li', class_='breadcrumb__item')
     div = soup.find_all('div', class_='specification__wrapper')
-    a = soup.find_all('section', class_='tab__panel tab__panel--icon')
+    a = soup.find('section', class_='tab__panel tab__panel--icon').find_all('a', class_='tab__icon')
 
     datas = []
     chapters = [i.get_text() for i in li]
@@ -85,26 +97,30 @@ def get_content(url):
         'URL': url,
         #'Files': files,
     })
-    # save_to_csv(datas)
+    save_to_csv(datas)
 
     files = []
     for i in a:
         files.append(
-            i.find('a', class_='tab__icon').get('href')
+            i.get('href')
         )
 
-    f_n = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(5))
-    id = datas[0]['Chapters'][-1].split(' ')[-1]
-    file_name = id + '_' + f_n
+    
     for url in files:
+        f_n = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(5))
+        id = datas[0]['Chapters'][-1]
+        file_name = id + '_' + f_n
         save_url(url, file_name)
 
 
 def main():
     urls = read_csv(PATH)
-    print(len(urls))
-    for url in urls:
-        get_content(url)
+    # print(len(urls))
+    # for url in urls:
+    #     get_content(url)
+
+    with Pool(cpu_count()) as p:
+        p.map(get_content, urls)
 
 
 if __name__ == '__main__':
